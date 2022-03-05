00fd3c94af584789dad8b554a8ab2e7c5940fd9a
Mullins <mullinsatheem@gmail.com>
2022-03-05 18:11:53 +0200

    added Home Page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="navbar navbar-expand-lg  ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img class="logo" src="../assets/logo.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="About">About</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Encyclopedia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Shop</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Support</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav dr mb-2 mb-lg-0">
+         <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-circle"></i>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.dr{
+  margin-right: auto;
+  margin-left: auto;
+}
+.navbar{
+  background-color: #003459 ;
+  position: sticky;
+  top: 0;
+}
+.logo{
+  height: 50px;
+}
+.nav-link {
+
+  margin-left: 10%;
+  color: white;
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-
-
-  </div>
+  <section class="Home">
+   <video autoplay muted loop id="myVideo">
+  <source src="../assets/MessyViciousArchaeocete.mp4" type="video/mp4">
+</video>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Marine Life Encyclopedia</h1>
+        <p>Explore the Marine Life Encyclopedia to learn fun facts and more about your favorite marine animals. From sharks and sea turtles to ecosystems and corals, you’re in the right place to take a deep dive into life under the sea.</p>
+        <button class="button">Encyclopedia</button>
+        <button class="button">Shop</button>
+      </div>
+    </div>
+  </div> 
+  </section>
 </template>
 
 <script>
@@ -15,3 +26,31 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.button{
+  margin-left: 30px;
+}
+.container{
+  color: white;
+   position: absolute;
+   text-align: center;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    z-index: -1;
+}
+.Home{
+
+
+}
+#myVideo {
+  z-index: -1000;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+</style>
